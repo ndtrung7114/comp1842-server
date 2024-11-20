5e593520681d229926dae0263850635b903b2c2b
Duc Trung <trungnd7114@gmail.com>
2024-11-20 10:33:59 +0700

    fixt litte cors

--- config/cors.js
+++ config/cors.js
@@ -11,7 +11,7 @@ const allowedOrigins = require("./allowed_origins");
 // }
 
 const corsOptions = {
-  origin: "https://greenwichsocial.onrender.com",
+  origin: "https://greenwichsocial.vercel.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
